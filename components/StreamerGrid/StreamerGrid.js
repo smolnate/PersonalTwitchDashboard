faea626fbabf2c832345455abb746cd7e1e6cc3d
nty4 <46460284+smolnate@users.noreply.github.com>
2021-09-14 15:59:38 +0000

    style added for remove button on top right

--- components/StreamerGrid/StreamerGrid.js
+++ components/StreamerGrid/StreamerGrid.js
@@ -12,17 +12,25 @@ const StreamerGrid = ({channels}) => {
   )
 }
 
+//Actions
+const removeChannelAction = () => {
+
+}
+
 //Render Method
 const renderGridItem = channel => (
   <div key={channel.id} className={styles.gridItem}>
     <Image width='100px' height='100px' src={channel.thumbnail_url} />
     <div className={styles.gridItemContent}>
+      <button onClick={removeChannelAction(channel.id)}>X</button>
       <p>{'Channel Name: ' + channel.display_name}</p>
       {channel.is_live && <a href={'http://twitch.tv/' + channel.broadcaster_login}> Channel Status: 🟢 Live Now! Click to watch </a>}
       {!channel.is_live && <p>Channel Status: 🔴 Offline :(</p>}
     </div>
   </div>
 )
+
+
 
 
 export default StreamerGrid
