0731e7d00f5c7c60392238b3fbe95ebe90a423c0
nty4 <46460284+smolnate@users.noreply.github.com>
2021-09-15 01:02:40 +0000

    finished minimum viable product! onto adding more things!

--- components/StreamerGrid/StreamerGrid.js
+++ components/StreamerGrid/StreamerGrid.js
@@ -12,9 +12,9 @@ const StreamerGrid = ({ channels, setChannels }) => {
       <div className={styles.gridItemContent}>
         <button onClick={removeChannelAction(channel.id)}>X</button>
         <p style={{fontWeight: "bold", fontSize: "16px"}}>{channel.display_name}</p>
-        <p style={{color: "grey"}}>{channel.game_name}</p>
-        <p style={{color: "grey", fontSize: "12px"}}>{channel.title}</p>
-        {channel.is_live && <a style={{fontSize: "12px"}} href={'http://twitch.tv/' + channel.broadcaster_login}> 🟢 Live Now! 🔗 </a>}
+        <p style={{fontWeight: "bold", color: "grey"}}>{channel.game_name}</p>
+        <p style={{fontStyle: "italic", color: "grey", fontSize: "12px"}}>{channel.title}</p>
+        {channel.is_live && <a style={{fontSize: "12px"}} href={'http://twitch.tv/' + channel.broadcaster_login}> 🟢 Live Now! Click to watch 🔗 </a>}
         {!channel.is_live && <p style={{fontSize: "12px"}}>🔴 Offline</p>}
       </div>
     </div>
@@ -27,10 +27,37 @@ const StreamerGrid = ({ channels, setChannels }) => {
   )
 
   //Actions
-  const removeChannelAction = channelId => () => {
-    console.log("removing channel...")
-    setChannels(channels.filter(channel => channel.id != channelId))
+  const removeChannelAction = channelId => async () => {
+    console.log("removing channel with id: ", channelId)
+
+    const filteredChannels = channels.filter(channel => channel.id != channelId)
+    
+    setChannels(filteredChannels)
+
+    const joinedChannels = filteredChannels.map(channel => channel.display_name.toLowerCase()).join(",")
+
+    await setDBChannels(joinedChannels)
   } 
+
+  const setDBChannels = async channels => {
+    try {
+      const path = `https://${window.location.hostname}`
+
+      const response = await fetch (`${path}/api/database`, {
+        method: 'POST',
+        body: JSON.stringify({
+          key: 'CHANNELS',
+          value: channels
+        })
+      })
+
+      if (response.status === 200) {
+        console.log(`Set ${channels} in DB.`)
+      }
+    } catch (error) {
+      console.warn(error.message)
+    }
+  }
 
 
   return (
